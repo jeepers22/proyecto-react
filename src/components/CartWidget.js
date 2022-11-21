a45e5b472fe83df65c2d0bb51e87dbe1612c6417
Maximiliano Safran <maxizero@gmail.com>
2022-11-21 01:36:17 -0300

    Inicio consigna Pre Entrega

---
+++ src/components/CartWidget.js
@@ -0,0 +1,7 @@
+const CartWidget = () => {
+    return (
+        <img class="logo-carrito" src="https://img.icons8.com/ios/50/000000/shopping-cart-loaded--v2.png"/>
+    )
+}
+
+export default CartWidget
---
+++ src/components/NavBar.js
@@ -0,0 +1,15 @@
+import CartWidget from "./CartWidget"
+
+const NavBar = () => {
+    return (
+        <div>
+            <spam>Inicio</spam>
+            <spam>Deportes</spam>
+            <spam>Juventud</spam>
+            <spam>Contacto</spam>
+            <CartWidget />
+        </div>
+    )
+}
+
+export default NavBar
